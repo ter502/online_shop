ebfc4e7f68bd15079792ed4385ae23cc06570526
ter502 <linyute.ter@gamil.com>
2025-10-30 10:36:33 +0800

    create entity user

---
+++ backend_springboot/src/main/java/com/ter/online_shop/entity/User.java
@@ -0,0 +1,57 @@
+package com.ter.online_shop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="firstName")
+    private String fname;
+
+    @Column(name="lastName")
+    private String lname;
+
+    public User() {
+    }
+
+    public User(String fname, String lname) {
+        this.fname = fname;
+        this.lname = lname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+
+
+}
