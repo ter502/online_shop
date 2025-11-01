7e9e67e52c3d6c1139f21d8028b2201f3fced91c
ter502 <linyute.ter@gamil.com>
2025-11-02 02:14:45 +0800

    solve Cross-Origin Resource Sharing

--- backend_springboot/src/main/java/com/ter/online_shop/controller/UserController.java
+++ backend_springboot/src/main/java/com/ter/online_shop/controller/UserController.java
@@ -3,6 +3,7 @@ package com.ter.online_shop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ter.online_shop.entity.User;
-import com.ter.online_shop.repository.UserRepository;
 import com.ter.online_shop.services.UserService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     private UserService userService;
 
